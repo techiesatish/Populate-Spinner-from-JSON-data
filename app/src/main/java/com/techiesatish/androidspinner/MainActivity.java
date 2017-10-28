ba2f421c774def3b1d5a2775166eaf4f78e45fbe
Ansh <ansh0437@gmail.com>
2017-10-28 22:06:43 +0530

    fafda

---
+++ app/src/main/java/com/techiesatish/androidspinner/MainActivity.java
@@ -0,0 +1,83 @@
+package com.techiesatish.androidspinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+Spinner spinner;
+String URL="http://techiesatish.com/demo_api/spinner.php";
+ArrayList<String> CountryName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CountryName=new ArrayList<>();
+        spinner=(Spinner)findViewById(R.id.country_Name);
+        loadSpinnerData(URL);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+             String country=   spinner.getItemAtPosition(spinner.getSelectedItemPosition()).toString();
+            Toast.makeText(getApplicationContext(),country,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // DO Nothing here
+            }
+        });
+
+    }
+
+    private void loadSpinnerData(String url) {
+    RequestQueue requestQueue=Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject=new JSONObject(response);
+                    if(jsonObject.getInt("success")==1){
+                     JSONArray jsonArray=jsonObject.getJSONArray("Name");
+                     for(int i=0;i<jsonArray.length();i++){
+                     JSONObject jsonObject1=jsonArray.getJSONObject(i);
+                     String country=jsonObject1.getString("Country");
+                     CountryName.add(country);
+                     }
+                    }
+                    spinner.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, CountryName));
+                }catch (JSONException e){e.printStackTrace();}
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        int socketTimeout = 30000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        requestQueue.add(stringRequest);
+    }
+
+}
